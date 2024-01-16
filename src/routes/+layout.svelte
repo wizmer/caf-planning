4d6b17dd6df9f5dea478743c33426f6e1611ec91
Benoît Coste <benoit.coste@pm.me>
2024-01-16 06:47:10 +0100

    second commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,29 @@
-<script lang="ts">
+<script>
+	// Most of your app wide CSS should be put in this file
+	import { dev, browser } from '$app/environment';
 	import '../app.postcss';
-
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { page } from '$app/stores';
+	import { base } from '$app/paths';
+	import { AppShell, AppBar, LightSwitch, Toast, initializeStores } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+ /* 	import favicon from '$lib/assets/favicon.png'; */
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import '../app.postcss';
+	import { goto } from '$app/navigation';
+
+	initializeStores();
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
 </script>
 
-<slot />
+<Toast />
+
+<AppShell>
+	  <svelte:fragment slot="header">
+        <AppBar>
+	      		<h1 class="h1">Planning des créneaux libres</h1>
+        </AppBar>
+	  </svelte:fragment>
+	  <slot />
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
- import Icon from "@iconify/svelte";
+  import Icon from "@iconify/svelte";
 
- let today = new Date();
+  let today = new Date();
 
- let year = today.getFullYear();
- let month = today.getMonth();
- let date = today.getDate();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+  let date = today.getDate();
 
- let slots = [];
+  let current_user = "benoit";
+
+  let edition_enabled = true;
+
+  let is_updating = false;
+  let new_start_point = 0;
+
+  let slots = [];
 
   for (let i = 0; i < 30; i++) {
     let day = new Date(year, month, date + i);
     if ([1, 3, 5].includes(day.getDay())) {
       let item = {
-          id: i,
+        id: i,
         day: day,
         month: day.toLocaleDateString(
           "fr",
@@ -47,101 +54,131 @@
   }
 
 
-  let refs = [
-      {name: 'benoit', range: [new Date(2024, 0, 14, 18, 30), new Date(2024, 0, 14, 21)]},
-      {name: 'pierre', range: [new Date(2024, 0, 14, 19, 30), new Date(2024, 0, 14, 22, 0)]},
-      {name: 'josiane', range: [new Date(2024, 0, 14, 19, 0), new Date(2024, 0, 14, 22, 0)]}]
+  let refs = {
+    benoit: { name: "benoit", range: [new Date(2024, 0, 14, 18, 30), new Date(2024, 0, 14, 21)] },
+    pierre: { name: "pierre", range: [new Date(2024, 0, 14, 19, 30), new Date(2024, 0, 14, 22, 0)] },
+    josiane: { name: "josiane", range: [new Date(2024, 0, 14, 19, 0), new Date(2024, 0, 14, 22, 0)] }
+  };
 
- for(let ref of refs){
-   ref['presence'] = []
-   let day = new Date(ref.range[0])
-   day.setHours(18, 0)
-   console.log(day)
-   for(let i=0;i<16; i++){
-     let newDateObj = new Date(day.getTime() + i*15*60000);
-     ref['presence'].push((newDateObj >= ref.range[0]) && (newDateObj < ref.range[1]))
-   }
- }
+  for (let ref of Object.values(refs)) {
+    ref["presence"] = [];
+    let day = new Date(ref.range[0]);
+    day.setHours(18, 0);
+    for (let i = 0; i < 16; i++) {
+      let newDateObj = new Date(day.getTime() + i * 15 * 60000);
+      ref["presence"].push((newDateObj >= ref.range[0]) && (newDateObj < ref.range[1]));
+    }
+  }
+
+  function start_update(i: number) {
+    if (!is_updating) {
+      refs[current_user].presence = Array(16).fill(false);
+      refs[current_user].presence[i] = true;
+      new_start_point = i;
+    } else {
+      for (let pos = Math.min(new_start_point, i); pos <= Math.max(new_start_point, i); pos++) {
+        refs[current_user].presence[pos] = true;
+      }
+    }
+    is_updating = !is_updating;
+  }
+
+  function delete_line() {
+    console.log('do you come here?')
+    refs[current_user].presence = Array(16).fill(false)
+    console.log(refs)
+  }
 
 </script>
-<!-- Responsive Container (recommended) -->
+
 <div class="table-container">
   <!-- Native Table Element -->
   <table class="table table-hover">
-    <thead>
-    <tr>
-      <th>Date</th>
-      <th>Plot</th>
-    </tr>
-    </thead>
     <tbody>
     {#each slots as row,i (row.id)}
       {#if (i === 0) || (slots[i - 1].day.getMonth() != slots[i].day.getMonth()) }
         <tr>
           <td colspan="2">
-              <div class="h2">
+            <div class="h2">
               {row.month}
-              </div>
+            </div>
           </td>
         </tr>
       {/if}
       <tr>
-          <td>
-              <div >{row.date}</div>
-          </td>
-        {#if row.status == 'no_referrent'}
-          <td class="no_referrent">
-              <div >
-            Pas de referent pour le moment
-              </div>
-          </td>
-        {:else if row.status == 'cancelled'}
-          <td class="cancelled">
-            <div
-              style="display: flex"
-                class="h3"
-            >
-              <Icon icon="mdi:alert" />
-              <div style="margin-left: 10px">Session annulee</div>
-            </div>
-          </td>
-        {:else}
-          <td>
-              <table >
-                  <thead>
-                      <td>Nom</td>
-                      <td colspan="4">18:00</td>
-                      <td colspan="4">19:00</td>
-                      <td colspan="4">20:00</td>
-                      <td colspan="4">21:00</td>
-                  </thead>
+        <td>
+          <div class="h3">{row.date}</div>
+        </td>
+        <td>
 
-                  <tbody>
-                  {#each refs as ref (ref.name)}
-                      <tr>
-                          <td>{ref.name}</td>
-                        {#each ref.presence as present}
-                          <td class:present={present}></td>
-                          {/each}
-                      </tr>
+          <div class="table-container">
+            <table class="table table-hover max-w-prose">
+              <thead class="divide-x">
+              <th>Nom</th>
+              <th colspan="4">18:00</th>
+              <th colspan="4">19:00</th>
+              <th colspan="4">20:00</th>
+              <th colspan="4">21:00</th>
+              </thead>
+
+              <tbody>
+
+              {#if row.status == 'no_referrent'}
+                <tr>
+                  <td class="no_referrent" colspan="16">
+                    <div class="h3">
+                      Pas de referent pour le moment
+                    </div>
+                  </td>
+                </tr>
+              {:else if row.status == 'cancelled'}
+                <tr>
+                  <td class="cancelled" colspan="16">
+                    <div
+                      class="h3"
+                    >
+                      Session annulee
+                    </div>
+                  </td>
+                </tr>
+              {:else}
+                {#each Object.values(refs) as ref}
+                  <tr class="py-0 divide-x divide-y">
+                    <td>
+                      {ref.name}
+                      {#if edition_enabled && ref.name == current_user}
+                        <button class="button" on:click={delete_line} >
+                          <Icon icon="ph:trash" style="color: red"
+                                />
+                        </button>
+                      {/if}
+
+                    </td>
+                    {#each ref.presence as present, i}
+                      <td class:present={present} on:click={()=>start_update(i, ref)}></td>
                     {/each}
-                  </tbody>
-          </td>
-        {/if}
+                  </tr>
+                {/each}
+              {/if}
+              </tbody>
+            </table>
+          </div>
+        </td>
       </tr>
     {/each}
     </tbody>
-
   </table>
 </div>
 
 <style>
-    thead{
-        /*text-align: left;*/
+    th {
+        text-align: left;
     }
-    .present{
+
+    .present {
         background-color: green;
     }
+
     .no_referrent {
         color: orange;
     }

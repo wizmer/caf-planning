d895951d37b4edcda7b47b87487d6c29e2ce178c
Benoît Coste <benoit.coste@pm.me>
2024-01-16 14:44:29 +0100

    keep

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,10 @@
 	// Most of your app wide CSS should be put in this file
 	import { dev, browser } from '$app/environment';
 	import '../app.postcss';
-	import { page } from '$app/stores';
-	import { base } from '$app/paths';
 	import { AppShell, AppBar, LightSwitch, Toast, initializeStores } from '@skeletonlabs/skeleton';
-	import Icon from '@iconify/svelte';
- /* 	import favicon from '$lib/assets/favicon.png'; */
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
-	import { goto } from '$app/navigation';
 
 	initializeStores();
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
@@ -21,8 +16,12 @@
 
 <AppShell>
 	  <svelte:fragment slot="header">
-        <AppBar>
-	      		<h1 class="h1">Planning des créneaux libres</h1>
+        <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	          <!-- <svelte:fragment slot="lead">(icon)</svelte:fragment> -->
+	      		<h1 class="h1 text-center">Planning des créneaux libres</h1>
+			     	<svelte:fragment slot="trail">
+                <LightSwitch />
+            </svelte:fragment>
         </AppBar>
 	  </svelte:fragment>
 	  <slot />

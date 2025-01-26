4ca0e01a72e2603d2507ace95c06b102bf1a084e
Benoît Coste <benoit.coste@pm.me>
2025-01-26 15:28:13 +0100

    Wrap db calls in a context that close connection
    even it it crashes

--- src/routes/api/slot/+server.ts
+++ src/routes/api/slot/+server.ts
@@ -1,54 +1,38 @@
 import { Connection } from 'postgresql-client';
 import type { RequestHandler } from './$types';
 
-import { DB_STRING } from '$env/static/private';
+import { connectionContext } from '../../../server/utils';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { day, ref } = await request.json();
+	return connectionContext(async (connection: Connection) => {
+		if (ref.start && ref.end) {
+			const name = await connection.query(`select id from referents where  name = '${ref.name}'`);
+			const ref_id = name.rows[0][0];
+			await connection.query(`delete from slots where ref_id = ${ref_id} and day = '${day}'`);
+			await connection.query(
+				`insert into slots values (DEFAULT, ${ref_id}, '${day}', '${ref.start}', '${ref.end}')`
+			);
 
-	if (ref.start && ref.end) {
-		const connection = new Connection(DB_STRING);
-		// Connect to database server
-		await connection.connect();
+			return new Response('ok');
+		} else if (!ref.start && !ref.end) {
+			const name = await connection.query(`select id from referents where  name = '${ref.name}'`);
+			const ref_id = name.rows[0][0];
+			await connection.query(`delete from slots where ref_id = ${ref_id} and day = '${day}'`);
 
-		// Execute query and fetch rows
-		const name = await connection.query(`select id from referents where  name = '${ref.name}'`);
-		const ref_id = name.rows[0][0];
-		await connection.query(`delete from slots where ref_id = ${ref_id} and day = '${day}'`);
-		await connection.query(
-			`insert into slots values (DEFAULT, ${ref_id}, '${day}', '${ref.start}', '${ref.end}')`
-		);
+			return new Response('ok');
+		}
 
-		// Disconnect from server
-		await connection.close();
-
-		return new Response('ok');
-	} else if (!ref.start && !ref.end) {
-		const connection = new Connection(DB_STRING);
-		// Connect to database server
-		await connection.connect();
-
-		// Execute query and fetch rows
-		const name = await connection.query(`select id from referents where  name = '${ref.name}'`);
-		const ref_id = name.rows[0][0];
-		await connection.query(`delete from slots where ref_id = ${ref_id} and day = '${day}'`);
-
-		// Disconnect from server
-		await connection.close();
-
-		return new Response('ok');
-	}
-
-	return new Response('error');
+		return new Response('error');
+	});
 };
 
 export const DELETE: RequestHandler = async ({ request }) => {
 	const { ref_id } = await request.json();
 
-	const connection = new Connection(DB_STRING);
-	await connection.connect();
-	await connection.query(`delete from slots where ref_id = ${ref_id}`);
-	await connection.query(`delete from referents where id = ${ref_id}`);
-	await connection.close();
-	return new Response('ok');
+	return connectionContext(async (connection: Connection) => {
+		await connection.query(`delete from slots where ref_id = ${ref_id}`);
+		await connection.query(`delete from referents where id = ${ref_id}`);
+		return new Response('ok');
+	});
 };

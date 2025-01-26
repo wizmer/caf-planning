60f4ac9dd01ec6e17a6f63092e8801bf313452fe
Benoît Coste <benoit.coste@pm.me>
2025-01-26 14:15:35 +0100

    do not show past events

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -28,7 +28,7 @@ export async function load() {
 	}
 
 	const referents = await connection.query('select * from referents');
-	const events = await connection.query('select * from events');
+	const events = await connection.query('select * from events where date(day) >= now()');
 
 	const events_groups = Object.fromEntries(events.rows.map((row) => [row[1], row]));
 

09cb172de910563f72edc39f72c3915b7a559212
Benoît Coste <benoit.coste@pm.me>
2025-11-11 20:41:10 +0100

    finish prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+	schema: 'prisma/schema.prisma',
+	migrations: {
+		path: 'prisma/migrations'
+	},
+	engine: 'classic',
+	datasource: {
+		url: env('DATABASE_URL')
+	}
 });

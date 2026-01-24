3690191dcfcf4439f7e3856381ead4d9d32d405d
Benoît Coste <benoit.coste@pm.me>
2026-01-24 20:47:38 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,5 @@ export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	// https://github.com/googlemaps/js-api-loader/issues/692#issuecomment-1399359795
-	ssr: {
-		noExternal: ['@googlemaps/js-api-loader']
 	}
 });

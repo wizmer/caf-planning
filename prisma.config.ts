9eae71d37ca9190c71e0b6fc4731c4f9b9f89a3b
Benoît Coste <benoit.coste@pm.me>
2026-01-30 22:58:23 +0100

    adding normal stuff

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,8 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
 	schema: 'prisma/schema.prisma',
 	migrations: {
-		path: 'prisma/migrations'
+		path: 'prisma/migrations',
+		seed: 'npx tsx prisma/seed.ts'
 	},
 	engine: 'classic',
 	datasource: {
